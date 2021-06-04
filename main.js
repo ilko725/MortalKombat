35b14cfba84049e9bb6a7d4e9e971f05e32351db
ilko725 <ilko725@gmail.com>
2021-06-04 12:01:39 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const $arenas = document.querySelector('.arenas');
+
 const player1 = {
     name: 'Scorpion',
     hp: 80,
@@ -16,48 +18,7 @@ const player2 = {
         console.log(player2.name + 'Fight...');
     },
 };
-/* Task #2
 
-*/
-
-/*
-function createPlayerTask2(playernumber, name, hp, img) {
-    const $player1 = document.createElement('div');
-    $player1.classList.add(playernumber);
-
-    const $progressbar = document.createElement('div');
-    $progressbar.classList.add('progressbar');
-
-    const $life = document.createElement('div');
-    $life.classList.add('life');
-    $life.style.width = hp + '%';
-
-    const $name = document.createElement('div');
-    $name.classList.add('name');
-    $name.innerText = name;
-
-    const $character = document.createElement('div');
-    $character.classList.add('character');
-
-    const $arenas = document.querySelector('.arenas');
-    const $img = document.createElement('img');
-    $img.src = img;
-
-    $arenas.appendChild($player1);
-    $player1.appendChild($progressbar);
-    $progressbar.appendChild($life);
-    $progressbar.appendChild($name);
-    $player1.appendChild($character);
-    $character.appendChild($img);
-};
-
-createPlayerTask2('player1', "SCORPION", 50, player1.img);
-createPlayerTask2('player2', "SUB-ZERO", 80, player2.img);
-*/
-
-/* Task #3
-
-*/
 function createPlayerTask3(playerposition, playerobject) {
     const $player = document.createElement('div');
     $player.classList.add(playerposition);
@@ -76,7 +37,7 @@ function createPlayerTask3(playerposition, playerobject) {
     const $character = document.createElement('div');
     $character.classList.add('character');
 
-    const $arenas = document.querySelector('.arenas');
+
     const $img = document.createElement('img');
     $img.src = playerobject.img;
 
